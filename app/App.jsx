efc62af29b1b3e9bac67d4f815584c5f5fbf7287
Michael Prather <mprather1@gmail.com>
2017-06-15 18:54:12 +0000

    style

--- app/App.jsx
+++ app/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 class App extends React.Component {
   render () {
+    var myStyle = {
+      fontSize: 100,
+      color: '#FF0000'
+    }
+
     return (
       <div>
-      Hello World!
+        <h1 style={myStyle}>Header</h1>
       </div>
     )
   }
