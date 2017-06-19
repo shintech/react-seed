0a8b567446416ee06a31e9832f588e8e59a5f1d4
Michael Prather <mprather1@gmail.com>
2017-06-19 14:37:16 +0000

    stateless example

--- app/App.jsx
+++ app/App.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 
 class App extends React.Component {
   render () {
-    var myStyle = {
-      fontSize: 100,
-      color: '#FF0000'
-    }
-
-    var i = 1
-
     return (
       <div>
-        <h1 style={myStyle}>Header</h1>
-        <p>{i === 1 ? 'True' : 'False'}</p>
+        <Header />
+        <Content />
       </div>
     )
+  }
+}
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>)
+  }
+}
+
+class Content extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>Content text....</p>
+      </div>)
   }
 }
 
