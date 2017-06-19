aae37e39031539d315062af6efde3e140aaa0816
Michael Prather <mprather1@gmail.com>
2017-06-19 15:04:29 +0000

    state example

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      data:
+      [
+        {
+          'id': 1,
+          'name': 'Foo',
+          'age': '20'
+        },
+
+        {
+          'id': 2,
+          'name': 'Bar',
+          'age': '30'
+        },
+
+        {
+          'id': 3,
+          'name': 'Baz',
+          'age': '40'
+        }
+      ]
+    }
+  }
+
   render () {
     return (
       <div>
         <Header />
-        <Content />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+          </tbody>
+        </table>
       </div>
     )
   }
@@ -13,20 +51,28 @@ class App extends React.Component {
 
 class Header extends React.Component {
   render () {
+    var myStyle = {
+      fontSize: 35,
+      color: '#FF0000'
+    }
+
     return (
       <div>
-        <h1>Header</h1>
-      </div>)
+        <h1 style={myStyle}>Header</h1>
+      </div>
+    )
   }
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component {
   render () {
     return (
-      <div>
-        <h2>Content</h2>
-        <p>Content text....</p>
-      </div>)
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    )
   }
 }
 
