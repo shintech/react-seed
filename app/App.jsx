9226a37ea1e20554db6ccc9f7e943d226992c41b
Michael Prather <mprather1@gmail.com>
2017-06-19 15:19:28 +0000

    state and props

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
 
     this.state = {
+      header: 'Header',
+      content: 'Page Content...',
       data:
       [
         {
@@ -31,7 +33,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <Header />
+        <Header headerProp={this.state.header} />
         <table>
           <thead>
             <tr>
@@ -44,6 +46,9 @@ class App extends React.Component {
             {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
           </tbody>
         </table>
+        <div>
+          <Content contentProp={this.state.content} />
+        </div>
       </div>
     )
   }
@@ -58,7 +63,7 @@ class Header extends React.Component {
 
     return (
       <div>
-        <h1 style={myStyle}>Header</h1>
+        <h1 style={myStyle}>{this.props.headerProp}</h1>
       </div>
     )
   }
@@ -72,6 +77,16 @@ class TableRow extends React.Component {
         <td>{this.props.data.name}</td>
         <td>{this.props.data.age}</td>
       </tr>
+    )
+  }
+}
+
+class Content extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>{this.props.contentProp}</p>
+      </div>
     )
   }
 }
